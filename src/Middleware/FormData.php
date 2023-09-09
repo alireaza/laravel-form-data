5b60ccc06b1cfb988017f886fdcf0a5948425a60
AliReaza <alireazamagh@gmail.com>
2023-09-09 06:57:25 +0330

    Refactor FormData middleware to use Illuminate\Http\Request and improve file handling to handle nested arrays of files correctly

--- src/Middleware/FormData.php
+++ src/Middleware/FormData.php
@@ -2,10 +2,11 @@
 
 namespace AliReaza\Laravel\Request\Middleware;
 
-use Closure;
-use Symfony\Component\HttpFoundation\Request;
 use AliReaza\Component\HttpFoundation\Request\FormData as FormDataHandler;
+use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
 
 class FormData
 {
@@ -15,28 +16,37 @@ class FormData
         Request::METHOD_POST,
     ];
 
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        if ($request instanceof Request) {
-            if (!in_array($request->getRealMethod(), $this->disallowMethods)) {
-                $headers = $request->headers;
-                $contentType = $headers->get('content-type');
+        if (!in_array($request->getRealMethod(), $this->disallowMethods)) {
+            $headers = $request->headers;
+            $contentType = $headers->get('content-type');
 
-                if (preg_match('/multipart\/form-data/', $contentType)) {
-                    $content = $request->getContent();
+            if (preg_match('/multipart\/form-data/', $contentType)) {
+                $content = $request->getContent();
 
-                    $static = new FormDataHandler($content);
+                $static = new FormDataHandler($content);
 
-                    $request->request->add($static->inputs);
+                $request->request->add($static->inputs);
 
-                    foreach ($static->files as $key => $file) {
-                        $file = new UploadedFile($file['tmp_name'], $file['name'], $file['type'], $file['error'], true);
-                        $request->files->set($key, $file);
-                    }
-                }
+                $request->files->add($static->files);
+
+                $files = $this->handleFiles($request->files->all());
+                $request->files->replace($files);
             }
         }
 
         return $next($request);
+    }
+
+    private function handleFiles(array $files): array
+    {
+        $result = [];
+
+        foreach ($files as $key => $file) {
+            $result[$key] = is_array($file) ? $this->handleFiles($file) : UploadedFile::createFromBase($file, true);
+        }
+
+        return $result;
     }
 }
